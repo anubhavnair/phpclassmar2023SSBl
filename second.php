77cbfbf1592d45b74624adab0afe1481eeaf32b5
sensible <sensiblemarg@gmail.com>
2023-05-05 08:45:08 +0530

    while loop concept

--- second.php
+++ second.php
@@ -8,12 +8,14 @@
      $b = $_REQUEST["t2"];
      $c = $_REQUEST["t3"];
 
-   if($a>$b && $a>$c)
-      print($a ." is greater ");
-  else if($b>$a && $b>$c)
-      print($b ." is greater ");
-  else if($c>$a && $c>$b)
-      print($c ." is greater ");
+     $i=$a;
+     while($i<=$b)
+     {
+         echo($i."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+
+         $i=$i+$c;
+
+     }
 
 
 
